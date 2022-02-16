17d9e8f6ff25d26eeeeb01d79934fb96e39341a2
Jeron <norejudeisa@gmail.com>
2022-02-16 13:32:35 -0800

    finished product details for mobile devices

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -12,6 +12,11 @@ import {
   ListItem,
   Text,
   VStack,
+  Heading,
+  useMediaQuery,
+  Wrap,
+  WrapItem,
+  IconButton,
 } from '@chakra-ui/react'
 import React from 'react'
 import { useSearchParams, useParams } from 'react-router-dom'
@@ -21,11 +26,17 @@ import {
   BsFlag,
   BiChevronRight,
   BiChevronLeft,
+  BiTime,
+  GoLocation,
+  BsTelephoneOutbound,
+  IoIosArrowForward,
 } from 'react-icons/all'
 import { useState } from 'react'
 const Cart = () => {
   const { name } = useParams()
   const [searchParams, setSearchParams] = useSearchParams()
+  const [mobile] = useMediaQuery('(max-width: 768px)')
+
   const id = searchParams.get('id')
   const images = [
     {
@@ -77,7 +88,7 @@ const Cart = () => {
   return (
     <Box w="100%" mb="3">
       <Flex w="100" h="100%" gap="2" direction={['column', 'row']}>
-        <Box flex="0.65">
+        <Box flex="0.65" shadow="md">
           <VStack w="100" h="100%">
             <Box h={['14rem', '24.5rem']} w="100%" pos="relative">
               <Image
@@ -111,7 +122,14 @@ const Cart = () => {
                 <BiChevronRight size={22} />
               </Button>
             </Box>
-            <HStack w="100%" flex="1" border="1px" borderColor="gray.300" p="1">
+            <HStack
+              w="100%"
+              flex="1"
+              border="1px"
+              borderColor="gray.300"
+              p="1"
+              h={['4rem', '7.2rem']}
+            >
               {images.map((image, idx) => (
                 <Box
                   w="20%"
@@ -134,63 +152,171 @@ const Cart = () => {
                 </Box>
               ))}
             </HStack>
+            <Box w="100%" p="3">
+              {mobile ? (
+                <VStack alignItems="flex-start" spacing="1">
+                  <Flex justifyContent="space-between" wrap="wrap" w="100%">
+                    <Heading size="md">{name}</Heading>
+                    <Heading size="md" color="blue.400">
+                      GH₵ 540
+                    </Heading>
+                  </Flex>
+                  <HStack spacing="1" fontSize="sm" color="gray.600">
+                    <GoLocation />
+                    <Text>Eastern Region, Atiwa</Text>
+                  </HStack>
+                  <HStack
+                    w="100%"
+                    justifyContent="space-between"
+                    colorScheme="blue"
+                  >
+                    <Button
+                      colorScheme="blue"
+                      variant="outline"
+                      size="sm"
+                      rounded="sm"
+                      leftIcon={<BiChat />}
+                    >
+                      Make an offer
+                    </Button>
+                    <Button
+                      colorScheme="blue"
+                      size="sm"
+                      leftIcon={<BsTelephoneOutbound />}
+                      rounded="sm"
+                    >
+                      Show Contact
+                    </Button>
+                  </HStack>
+                </VStack>
+              ) : (
+                <VStack alignItems="flex-start">
+                  <Heading size="md">{name}</Heading>
+                  <HStack spacing="2">
+                    <HStack spacing="1" fontSize="sm" color="gray.600">
+                      <BiTime />
+                      <Text>posted 1 day ago</Text>
+                    </HStack>
+                    <HStack spacing="1" fontSize="sm" color="gray.600">
+                      <GoLocation />
+                      <Text>Eastern Region, Atiwa</Text>
+                    </HStack>
+                  </HStack>
+                  <HStack>
+                    <Text>Major points of description:</Text>
+                  </HStack>
+                </VStack>
+              )}
+            </Box>
           </VStack>
         </Box>
         {/* RIGHT SIDE */}
         <Box flex="0.35">
           <VStack>
             {/* FIRST BOX */}
-            <VStack
-              shadow="md"
-              w="100%"
-              p="3"
-              alignItems={'flex-start'}
-              border="1px"
-              borderColor="gray.300"
-            >
-              <Text fontSize="3xl" fontWeight="600">
-                GH₵ 540
-              </Text>
-              <Button w="100%" colorScheme="blue" variant="outline">
-                Make an offer
-              </Button>
-            </VStack>
-            {/* SECOND BOX */}
-            <VStack
-              shadow="md"
-              w="100%"
-              p="3"
-              alignItems={'flex-start'}
-              border="1px"
-              borderColor="gray.300"
-            >
-              <HStack>
-                <Avatar>
-                  <AvatarBadge
-                    boxSize="1em"
-                    bg="green.500"
-                    borderColor="green.100"
-                  />
-                </Avatar>
-                <VStack alignSelf="flex-start" fontWeight="600" spacing="1">
-                  <Text>Jonathan Day</Text>
-                  <Badge alignSelf="flex-start" fontSize="0.6em">
-                    2y 7m on J-Mart
-                  </Badge>
-                </VStack>
-              </HStack>
-              <Button w="100%" colorScheme="blue">
-                Show Contact
-              </Button>
-              <Button
+            {mobile ? (
+              <Box
+                shadow="md"
                 w="100%"
-                colorScheme="blue"
-                variant="outline"
-                leftIcon={<BiChat />}
+                p="3"
+                border="1px"
+                borderColor="gray.300"
               >
-                Start Chat
-              </Button>
-            </VStack>
+                <Text>Some super description</Text>
+              </Box>
+            ) : (
+              <VStack
+                shadow="md"
+                w="100%"
+                p="3"
+                alignItems={'flex-start'}
+                border="1px"
+                borderColor="gray.300"
+              >
+                <Text fontSize="3xl" fontWeight="600">
+                  GH₵ 540
+                </Text>
+                <Button
+                  w="100%"
+                  colorScheme="blue"
+                  variant="outline"
+                  rounded="sm"
+                >
+                  Make an offer
+                </Button>
+              </VStack>
+            )}
+
+            {/* SECOND BOX */}
+            {mobile ? (
+              <Flex
+                justify="space-between"
+                shadow="md"
+                w="100%"
+                p="3"
+                border="1px"
+                borderColor="gray.300"
+              >
+                <HStack spacing="4">
+                  <Avatar>
+                    <AvatarBadge
+                      boxSize="1em"
+                      bg="green.500"
+                      borderColor="green.100"
+                    />
+                  </Avatar>
+                  <VStack alignSelf="flex-start" fontWeight="600" spacing="1">
+                    <Text>Jonathan Day</Text>
+                    <Badge alignSelf="flex-start" fontSize="0.6em">
+                      2y 7m on J-Mart
+                    </Badge>
+                  </VStack>
+                </HStack>
+                <IconButton
+                  icon={<IoIosArrowForward size={20} />}
+                  size="sm"
+                  variant="ghost"
+                />
+              </Flex>
+            ) : (
+              <VStack
+                shadow="md"
+                w="100%"
+                p="3"
+                alignItems={'flex-start'}
+                border="1px"
+                borderColor="gray.300"
+              >
+                <HStack>
+                  <Avatar>
+                    <AvatarBadge
+                      boxSize="1em"
+                      bg="green.500"
+                      borderColor="green.100"
+                    />
+                  </Avatar>
+                  <VStack alignSelf="flex-start" fontWeight="600" spacing="1">
+                    <Text>Jonathan Day</Text>
+                    <Badge alignSelf="flex-start" fontSize="0.6em">
+                      2y 7m on J-Mart
+                    </Badge>
+                  </VStack>
+                </HStack>
+                <Button w="100%" colorScheme="blue" rounded="sm">
+                  Show Contact
+                </Button>
+                <Button
+                  w="100%"
+                  colorScheme="blue"
+                  variant="outline"
+                  leftIcon={<BiChat />}
+                  rounded="sm"
+                >
+                  Start Chat
+                </Button>
+              </VStack>
+            )}
+
             {/* THIRD BOX */}
             <VStack
               shadow="md"
@@ -214,6 +340,16 @@ const Cart = () => {
                 Report Abuse
               </Button>
             </VStack>
+            <Box p="3" w="100%" shadow="md" border="1px" borderColor="gray.300">
+              <Button
+                variant="outline"
+                colorScheme="blue"
+                w="100%"
+                rounded="sm"
+              >
+                Post Ads like this
+              </Button>
+            </Box>
           </VStack>
         </Box>
       </Flex>
