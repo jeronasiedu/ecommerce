25308d1b5a4c01de602f90207a4c91228e7cf6d3
Jeron <norejudeisa@gmail.com>
2022-03-12 05:45:41 +0000

    fixed something

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,6 +1,6 @@
 import create from 'zustand'
 const useStore = create((set, get) => ({
-  user: true,
+  user: false,
   updateUser: () => set((state) => ({ user: true })),
   isOpen: false,
   onOpen: () => set((state) => ({ isOpen: true })),
